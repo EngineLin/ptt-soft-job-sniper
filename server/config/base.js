728d5695c21aeeabd0538acdf1dc4f63616d99f3
EngineLin <30394570+EngineLin@users.noreply.github.com>
2018-03-26 16:00:51 +0800

    Delete base.js

--- server/config/base.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'mongodb init',
-  dbname:
-    'mongodb://<dbuser>:<dbpassword>@ds121089.mlab.com:21089/pttjobsniper_db',
-  dbuser: 'linengine',
-  dbpassword: 'mongodb54682',
-  secret: 'my_secret_key',
-};
